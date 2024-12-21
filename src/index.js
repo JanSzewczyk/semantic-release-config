c48a6ec4b3115279f627a2da7a951f1ea311034b
JanSzewczyk <jan.szewczyk1997@gmail.com>
2024-12-21 16:56:41 +0100

    feat(config): update notes generator types configuration

--- src/index.js
+++ src/index.js
@@ -26,12 +26,12 @@ export function getConfig({ features: { npmPublish = false } = {} } = {}) {
               { type: "perf", section: "Performance Improvements" },
               { type: "revert", section: "Reverts" },
               { type: "docs", section: "Documentation" },
-              { type: "style", section: "Styles", hidden: true },
-              { type: "chore", section: "Miscellaneous Chores", hidden: true },
-              { type: "refactor", section: "Code Refactoring", hidden: true },
-              { type: "test", section: "Tests", hidden: true },
-              { type: "build", section: "Build System", hidden: true },
-              { type: "ci", section: "Continuous Integration", hidden: true }
+              { type: "chore", section: "Miscellaneous Chores" },
+              { type: "refactor", section: "Code Refactoring" },
+              { type: "test", section: "Tests" },
+              { type: "build", section: "Build System" },
+              { type: "ci", section: "Continuous Integration" },
+              { type: "style", section: "Styles", hidden: true }
             ]
           }
         }
